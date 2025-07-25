ba12e7be2c599d258c4d38b56f819c17bb3394a6
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-07-25 19:16:24 -0400

    feat: Added theme toggler component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from './header/header';
 import { Intro } from './intro/intro';
 import { NoticeBoard } from './notice-board/notice-board';
 import { Footer } from './footer/footer';
+import { ToggleThemeButton } from './toggle-theme-button/toggle-theme-button';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, Header, Intro, NoticeBoard, Footer ],
+  imports: [ RouterOutlet, Header, Intro, NoticeBoard, Footer, ToggleThemeButton ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- frontend/src/app/header/header.ts
+++ frontend/src/app/header/header.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
-  toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    document.body.classList.toggle('light-mode');
-  }
+
 }
