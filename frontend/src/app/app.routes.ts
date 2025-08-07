d3768a0b6cccc4f88f4d1a83f5a00154b4560550
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-07 19:00:10 -0400

    refactor: Added routing for new pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NoticeArticle } from './notice-article/notice-article';
 import { AboutPage } from './about-page/about-page';
 import { Intro } from './intro/intro';
+import { NoticesPage } from './notices-page/notices-page';
 
 export const routes: Routes = [
   { path: '', component: Intro },
   { path: 'notice/:id', component: NoticeArticle },
-  { path: 'about', component: AboutPage }
+  { path: 'about', component: AboutPage },
+  { path: 'notices', component: NoticesPage }
 ];
