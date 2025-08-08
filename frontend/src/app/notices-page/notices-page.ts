31e53130263884a7cdbb395cf8921dd7c5611bfe
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-07 20:27:45 -0400

    feat: Added notices content

--- frontend/src/app/notices-page/notices-page.ts
+++ frontend/src/app/notices-page/notices-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NoticeBoard } from '../notice-board/notice-board';
 
 @Component({
   selector: 'app-notices-page',
-  imports: [],
+  imports: [
+    NoticeBoard
+  ],
   templateUrl: './notices-page.html',
   styleUrl: './notices-page.scss'
 })
