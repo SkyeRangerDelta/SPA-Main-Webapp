d007759999d219ac11fa60f5913fa02316e1920c
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-13 16:48:36 -0400

    feat: Added nav items for Regulations, Eprocs, and Services

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,17 @@ import { AboutPage } from './about-page/about-page';
 import { Intro } from './intro/intro';
 import { NoticesPage } from './notices-page/notices-page';
 import { Contact } from './contact/contact';
+import { Eprocs } from './eprocs/eprocs';
+import { Services } from './services/services';
+import { Regulations } from './regulations/regulations';
 
 export const routes: Routes = [
   { path: '', component: Intro },
   { path: 'notice/:id', component: NoticeArticle },
   { path: 'about', component: AboutPage },
   { path: 'notices', component: NoticesPage },
-  { path: 'contact', component: Contact }
+  { path: 'contact', component: Contact },
+  { path: 'eprocs', component: Eprocs },
+  { path: 'services', component: Services },
+  { path: 'regulations', component: Regulations }
 ];
