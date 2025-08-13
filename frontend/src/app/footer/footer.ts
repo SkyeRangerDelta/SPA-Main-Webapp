6d9fbd0d60bd9ae54bbcf8047d4a289d0e78c401
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-13 16:38:55 -0400

    refactor: Updated footer router links

--- frontend/src/app/footer/footer.ts
+++ frontend/src/app/footer/footer.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
-  baseLink = 'https://sarimportauthority.org';
   footerALinks = [
-    { label: 'About the Authority', url: `${this.baseLink}/about` },
-    { label: 'Regulations', url: `${this.baseLink}/regulations` },
-    { label: 'Public Notices', url: `${this.baseLink}/notices` }
+    { label: 'About the Authority', url: '/about' },
+    { label: 'Regulations', url: '/regulations' },
+    { label: 'Public Notices', url: '/notices' }
   ];
 
   footerBLinks = [
-    { label: 'Contact Us', url: `${this.baseLink}/contact` },
-    { label: 'Emergency Procedures', url: `${this.baseLink}/eprocs` },
-    { label: 'Harbour Services', url: `${this.baseLink}/harbourservices` }
+    { label: 'Contact Us', url: '/contact' },
+    { label: 'Emergency Procedures', url: '/eprocs' },
+    { label: 'Harbour Services', url: '/harbourservices' }
   ];
 }
